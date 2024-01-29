6e03324f71aeb124fa31f09580a255f66bb2a78f
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-01-29 17:20:51 -0500

    chore: update entrypoints

--- platform_plugin_turnitin/apps.py
+++ platform_plugin_turnitin/apps.py
@@ -4,6 +4,11 @@ platform_plugin_turnitin Django application initialization.
 
 from django.apps import AppConfig
 
+try:
+    from openedx.core.constants import COURSE_ID_PATTERN
+except ImportError:
+    COURSE_ID_PATTERN = object
+
 
 class PlatformPluginTurnitinConfig(AppConfig):
     """
@@ -11,15 +16,18 @@ class PlatformPluginTurnitinConfig(AppConfig):
     """
 
     name = "platform_plugin_turnitin"
+    verbose_name = "Platform Plugin Turnitin"
 
     plugin_app = {
+        "url_config": {
+            "lms.djangoapp": {
+                "namespace": "platform-plugin-turnitin",
+                "regex": rf"platform-plugin-turnitin/{COURSE_ID_PATTERN}/",
+                "relative_path": "urls",
+            },
+        },
         "settings_config": {
             "lms.djangoapp": {
-                "common": {"relative_path": "settings.common"},
-                "test": {"relative_path": "settings.test"},
-                "production": {"relative_path": "settings.production"},
-            },
-            "cms.djangoapp": {
                 "common": {"relative_path": "settings.common"},
                 "test": {"relative_path": "settings.test"},
                 "production": {"relative_path": "settings.production"},
