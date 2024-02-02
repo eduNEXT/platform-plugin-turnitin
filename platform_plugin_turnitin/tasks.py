041640a7873b6742379e5fac3a0b5090fd1a95e5
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-02-02 15:26:00 -0500

    feat: upload only files with valid extension

--- platform_plugin_turnitin/tasks.py
+++ platform_plugin_turnitin/tasks.py
@@ -1,16 +1,16 @@
-"""This file contains the tasks that will be run by celery."""
+"""This module contains the tasks that will be run by celery."""
 
 import tempfile
 from urllib.parse import urljoin
 
 import requests
-
 from celery import shared_task
 from django.conf import settings
 from rest_framework import status
 from submissions import api as submissions_api
 
 from platform_plugin_turnitin.api.v1.views import TurnitinClient
+from platform_plugin_turnitin.constants import ALLOWED_FILE_EXTENSIONS
 from platform_plugin_turnitin.edxapp_wrapper import user_by_anonymous_id
 
 
@@ -23,8 +23,6 @@ def ora_submission_created_task(submission_id: str, file_downloads: list) -> Non
         submission_id (str): The ORA submission ID.
         file_downloads (list): The list of file downloads.
     """
-    print(f"\n\nORA Submission ID: {submission_id}\n\n")
-
     submission_data = dict(submissions_api.get_submission_and_student(submission_id))
     user = user_by_anonymous_id(submission_data["student_item"]["student_id"])
 
@@ -63,17 +61,18 @@ def send_uploaded_files_to_turnitin(
     base_url = getattr(settings, "LMS_ROOT_URL", "")
 
     for file in file_downloads:
-        file_link = urljoin(base_url, file.get("download_url"))
-        response = requests.get(file_link, timeout=5)
+        if file.get("name").split(".")[-1] in ALLOWED_FILE_EXTENSIONS:
+            file_link = urljoin(base_url, file.get("download_url"))
+            response = requests.get(file_link, timeout=5)
 
-        if response.status_code == status.HTTP_200_OK:
-            with tempfile.NamedTemporaryFile() as temp_file:
-                temp_file.write(response.content)
-                temp_file.seek(0)
-                temp_file.name = file.get("name")
-                upload_turnitin_submission(submission_id, user, temp_file)
-        else:
-            raise Exception(f"Failed to download file from {file_link}")
+            if response.status_code == status.HTTP_200_OK:
+                with tempfile.NamedTemporaryFile() as temp_file:
+                    temp_file.write(response.content)
+                    temp_file.seek(0)
+                    temp_file.name = file.get("name")
+                    upload_turnitin_submission(submission_id, user, temp_file)
+            else:
+                raise Exception(f"Failed to download file from {file_link}")
 
 
 def upload_turnitin_submission(submission_id: str, user, file) -> None:
