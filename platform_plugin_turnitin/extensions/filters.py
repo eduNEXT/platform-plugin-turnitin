0e684c2a9e3983ac88e0c58731077ec4997eef60
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-02-06 18:56:48 -0500

    chore: remove context argument in filter

--- platform_plugin_turnitin/extensions/filters.py
+++ platform_plugin_turnitin/extensions/filters.py
@@ -7,20 +7,18 @@ class CheckTurnitinForPlagiarism(PipelineStep):
     """Check if Turnitin plagiarism check is enabled."""
 
     def run_filter(
-        self, context: dict, template_name: str
+        self, template_name: str
     ):  # pylint: disable=unused-argument, arguments-differ
         """
         Execute filter that loads the template with a warning message that notifies the user
         that the Turnitin plagiarism check is enabled.
 
         Args:
-            context (dict): ORA context.
             template_name (str): ORA template name.
 
         Returns:
-            dict: The context with the new template name.
+            dict: The new template name.
         """
         return {
-            "context": context,
             "template_name": "turnitin/oa_response.html",
         }
