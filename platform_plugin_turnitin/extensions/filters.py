e682b1056cde0b0031dca6de0be1f5de64619bd8
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-02-05 22:16:51 -0500

    feat: update turnitin filter

--- platform_plugin_turnitin/extensions/filters.py
+++ platform_plugin_turnitin/extensions/filters.py
@@ -1,9 +1,25 @@
+"""Filters for the Turnitin plugin."""
+
 from openedx_filters import PipelineStep
 
-class CheckTurnitinForPlagiarism(PipelineStep):
 
-    def run_filter(self, context, template_name):
-        print("CheckTurnitinForPlagiarism with context: %s" % context)
+class CheckTurnitinForPlagiarism(PipelineStep):
+    """Check if Turnitin plagiarism check is enabled."""
+
+    def run_filter(
+        self, context: dict, template_name: str
+    ):  # pylint: disable=unused-argument, arguments-differ
+        """
+        Execute filter that loads the template with a warning message that notifies the user
+        that the Turnitin plagiarism check is enabled.
+
+        Args:
+            context (dict): ORA context.
+            template_name (str): ORA template name.
+
+        Returns:
+            dict: The context with the new template name.
+        """
         return {
             "context": context,
             "template_name": "turnitin/ora.html",
