570aab14cc9a85a1b71800fc4b2c732bb8790a83
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-02-07 10:52:05 -0500

    feat: add context to the filter

--- platform_plugin_turnitin/extensions/filters.py
+++ platform_plugin_turnitin/extensions/filters.py
@@ -6,19 +6,21 @@ from openedx_filters import PipelineStep
 class CheckTurnitinForPlagiarism(PipelineStep):
     """Check if Turnitin plagiarism check is enabled."""
 
-    def run_filter(
-        self, template_name: str
-    ):  # pylint: disable=unused-argument, arguments-differ
+    def run_filter(  # pylint: disable=unused-argument, disable=arguments-differ
+        self, context: dict, template_name: str
+    ) -> dict:
         """
         Execute filter that loads the template with a warning message that notifies the user
         that the Turnitin plagiarism check is enabled.
 
         Args:
+            context (dict): The context dictionary.
             template_name (str): ORA template name.
 
         Returns:
-            dict: The new template name.
+            dict: The context dictionary and the template name.
         """
         return {
+            "context": context,
             "template_name": "turnitin/oa_response.html",
         }
