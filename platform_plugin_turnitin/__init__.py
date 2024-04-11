b513df6fd20c6c36d0a6ac743dce9965127779c9
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2024-04-11 18:10:08 -0500

    chore: update requirements (#13)

--- platform_plugin_turnitin/__init__.py
+++ platform_plugin_turnitin/__init__.py
@@ -5,6 +5,6 @@ Open edX Django plugin for Turnitin integrations using Open edX Filters.
 import os
 from pathlib import Path
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
