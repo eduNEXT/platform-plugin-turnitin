ae995972f3a102a5a9fc3cbbaabed89c6db29f2d
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-05-07 18:08:24 -0500

    chore: add modulestore backend and ENABLE_TURNITIN_SUBMISSION setting

--- platform_plugin_turnitin/settings/production.py
+++ platform_plugin_turnitin/settings/production.py
@@ -10,6 +10,9 @@ def plugin_settings(settings):
     Set of plugin settings used by the Open Edx platform.
     More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
+    settings.ENABLE_TURNITIN_SUBMISSION = getattr(settings, "ENV_TOKENS", {}).get(
+        "ENABLE_TURNITIN_SUBMISSION", settings.ENABLE_TURNITIN_SUBMISSION
+    )
     settings.TURNITIN_TII_API_URL = getattr(settings, "ENV_TOKENS", {}).get(
         "TURNITIN_TII_API_URL", settings.TURNITIN_TII_API_URL
     )
@@ -26,9 +29,7 @@ def plugin_settings(settings):
         "TURNITIN_TCA_API_KEY", settings.TURNITIN_TCA_API_KEY
     )
 
-    settings.TURNITIN_SIMILARITY_REPORT_PAYLOAD = getattr(
-        settings, "ENV_TOKENS", {}
-    ).get(
+    settings.TURNITIN_SIMILARITY_REPORT_PAYLOAD = getattr(settings, "ENV_TOKENS", {}).get(
         "TURNITIN_SIMILARITY_REPORT_PAYLOAD",
         settings.TURNITIN_SIMILARITY_REPORT_PAYLOAD,
     )
@@ -36,22 +37,20 @@ def plugin_settings(settings):
     settings.TURNITIN_API_TIMEOUT = getattr(settings, "ENV_TOKENS", {}).get(
         "TURNITIN_API_TIMEOUT", settings.TURNITIN_API_TIMEOUT
     )
-    settings.PLATFORM_PLUGIN_TURNITIN_AUTHENTICATION_BACKEND = getattr(
-        settings, "ENV_TOKENS", {}
-    ).get(
+    settings.PLATFORM_PLUGIN_TURNITIN_AUTHENTICATION_BACKEND = getattr(settings, "ENV_TOKENS", {}).get(
         "PLATFORM_PLUGIN_TURNITIN_AUTHENTICATION_BACKEND",
         settings.PLATFORM_PLUGIN_TURNITIN_AUTHENTICATION_BACKEND,
     )
-    settings.PLATFORM_PLUGIN_TURNITIN_STUDENT_BACKEND = getattr(
-        settings, "ENV_TOKENS", {}
-    ).get(
+    settings.PLATFORM_PLUGIN_TURNITIN_STUDENT_BACKEND = getattr(settings, "ENV_TOKENS", {}).get(
         "PLATFORM_PLUGIN_TURNITIN_STUDENT_BACKEND",
         settings.PLATFORM_PLUGIN_TURNITIN_STUDENT_BACKEND,
     )
-    settings.PLATFORM_PLUGIN_TURNITIN_COURSE_OVERVIEWS_BACKEND = getattr(
-        settings, "ENV_TOKENS", {}
-    ).get(
+    settings.PLATFORM_PLUGIN_TURNITIN_COURSE_OVERVIEWS_BACKEND = getattr(settings, "ENV_TOKENS", {}).get(
         "PLATFORM_PLUGIN_TURNITIN_COURSE_OVERVIEWS_BACKEND",
         settings.PLATFORM_PLUGIN_TURNITIN_COURSE_OVERVIEWS_BACKEND,
+    )
+    settings.PLATFORM_PLUGIN_TURNITIN_MODULESTORE_BACKEND = getattr(settings, "ENV_TOKENS", {}).get(
+        "PLATFORM_PLUGIN_TURNITIN_MODULESTORE_BACKEND",
+        settings.PLATFORM_PLUGIN_TURNITIN_MODULESTORE_BACKEND,
     )
     settings.MAKO_TEMPLATE_DIRS_BASE.append(ROOT_DIRECTORY / "templates/turnitin")
