4c86a3b26f4439d441bc48fb63c1baaec8889d59
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-05-07 18:09:03 -0500

    feat: check if turnitin submission is enabled

--- platform_plugin_turnitin/extensions/filters.py
+++ platform_plugin_turnitin/extensions/filters.py
@@ -1,14 +1,17 @@
 """Filters for the Turnitin plugin."""
 
+from crum import get_current_request
+from django.conf import settings
+from opaque_keys.edx.keys import CourseKey
 from openedx_filters import PipelineStep
+
+from platform_plugin_turnitin.edxapp_wrapper.modulestore import modulestore
 
 
 class ORASubmissionViewTurnitinWarning(PipelineStep):
     """Add warning message about Turnitin to the ORA submission view."""
 
-    def run_filter(  # pylint: disable=unused-argument, disable=arguments-differ
-        self, context: dict, template_name: str
-    ) -> dict:
+    def run_filter(self, context: dict, template_name: str) -> dict:  # pylint: disable=arguments-differ
         """
         Execute filter that loads the submission template with a warning message that
         notifies the user that the submission will be sent to Turnitin.
@@ -20,7 +23,24 @@ class ORASubmissionViewTurnitinWarning(PipelineStep):
         Returns:
             dict: The context dictionary and the template name.
         """
+        if settings.ENABLE_TURNITIN_SUBMISSION:
+            return {
+                "context": context,
+                "template_name": "turnitin/oa_response.html",
+            }
+
+        course_id = get_current_request().resolver_match.kwargs.get("course_id")
+        course_key = CourseKey.from_string(course_id)
+        course_block = modulestore().get_course(course_key)
+        enable_in_course = course_block.other_course_settings.get("ENABLE_TURNITIN_SUBMISSION", False)
+
+        if enable_in_course:
+            return {
+                "context": context,
+                "template_name": "turnitin/oa_response.html",
+            }
+
         return {
             "context": context,
-            "template_name": "turnitin/oa_response.html",
+            "template_name": template_name,
         }
--- platform_plugin_turnitin/handlers.py
+++ platform_plugin_turnitin/handlers.py
@@ -1,15 +1,34 @@
 """Event handlers for the Turnitin plugin."""
 
+from crum import get_current_request
+from django.conf import settings
+from opaque_keys.edx.keys import CourseKey
+
+from platform_plugin_turnitin.edxapp_wrapper.modulestore import modulestore
 from platform_plugin_turnitin.tasks import ora_submission_created_task
 
 
-def ora_submission_created(submission, **kwargs):
+def ora_submission_created(submission, **kwargs):  # pylint: disable=unused-argument
     """
     Handle the ORA_SUBMISSION_CREATED event.
 
     Args:
         submission (ORASubmissionData): The ORA submission data.
     """
+    if settings.ENABLE_TURNITIN_SUBMISSION:
+        call_ora_submission_created_task(submission)
+        return
+
+    course_id = get_current_request().resolver_match.kwargs.get("course_id")
+    course_key = CourseKey.from_string(course_id)
+    course_block = modulestore().get_course(course_key)
+    enable_in_course = course_block.other_course_settings.get("ENABLE_TURNITIN_SUBMISSION", False)
+
+    if enable_in_course:
+        call_ora_submission_created_task(submission)
+
+
+def call_ora_submission_created_task(submission):
     ora_submission_created_task.delay(
         submission.uuid,
         submission.anonymous_user_id,
